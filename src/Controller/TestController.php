6fcb892d8c014e7342021e60c07d66435c5b0b8c
Roma Movchan <movchan@gmail.com>
2018-02-25 17:15:07 +0200

    token

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,13 +2,31 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends Controller
 {
+
+    /**
+     * @Route("/", name="homepage")
+     */
     public function test()
     {
         return $this->render('test/index.html.twig');
+    }
+
+    /**
+     * @Route("/fbbot", name="fbbot")
+     * @param Request $request
+     */
+    public function fbbotAction(Request $request)
+    {
+        $verify_token = "token"; // Verify token
+        if (!empty($_REQUEST['hub_mode']) && $_REQUEST['hub_mode'] == 'subscribe' && $_REQUEST['hub_verify_token'] == $verify_token) {
+            echo $_REQUEST['hub_challenge'];
+        }
     }
 }
