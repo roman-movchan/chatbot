66dbcff6879d2e54b86b5252683a3730573e8a66
Roma Movchan <movchan@gmail.com>
2018-02-25 19:01:23 +0200

    back

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -45,9 +45,11 @@ class TestController extends Controller
             return new Response($_REQUEST['hub_challenge']);
         } else {
 
-            $data = json_decode(file_get_contents("php://input"), true, 512, JSON_BIGINT_AS_STRING);
-            if (!empty($data['entry'][0]['messaging'])) {
-                foreach ($data['entry'][0]['messaging'] as $message) {
+            $data = json_decode(file_get_contents("php://input"), true);
+            if (!empty($data['entry'][0]['messaging']))
+            {
+                foreach ($data['entry'][0]['messaging'] as $message)
+                {
                     // Skipping delivery messages
                     if (!empty($message['delivery'])) {
                         continue;
@@ -56,122 +58,14 @@ class TestController extends Controller
                     if (($message['message']['is_echo'] == "true")) {
                         continue;
                     }
-                    $command = "";
-                    // When bot receive message from user
-                    if (!empty($message['message'])) {
-                        $command = trim($message['message']['text']);
-                        // When bot receive button click from user
-                    } else if (!empty($message['postback'])) {
-                        $text = "Postback received: ".trim($message['postback']['payload']);
-                        $bot->send(new Message($message['sender']['id'], $text));
-                        continue;
-                    }
-// Обрабатываем команду
-                    switch ($command) {
 
-                        // When bot receive "text"
-                        case 'text':
-                            $bot->send(new Message($message['sender']['id'], 'This is a simple text message.'));
-                            break;
+                    $bot->send(new Message($message['sender']['id'], 'Halo'));
 
-                        // When bot receive "button"
-                        case 'button':
-                            $bot->send(new StructuredMessage($message['sender']['id'],
-                                StructuredMessage::TYPE_BUTTON,
-                                [
-                                    'text' => 'Choose category',
-                                    'buttons' => [
-                                        new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
-                                        new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button'),
-                                        new MessageButton(MessageButton::TYPE_POSTBACK, 'Third button')
-                                    ]
-                                ]
-                            ));
-                            break;
-
-                        // When bot receive "generic"
-                        case 'generic':
-
-                            $bot->send(new StructuredMessage($message['sender']['id'],
-                                StructuredMessage::TYPE_GENERIC,
-                                [
-                                    'elements' => [
-                                        new MessageElement("First item", "Item description", "", [
-                                            new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
-                                            new MessageButton(MessageButton::TYPE_WEB, 'Web link', 'http://facebook.com')
-                                        ]),
-
-                                        new MessageElement("Second item", "Item description", "", [
-                                            new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
-                                            new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
-                                        ]),
-
-                                        new MessageElement("Third item", "Item description", "", [
-                                            new MessageButton(MessageButton::TYPE_POSTBACK, 'First button'),
-                                            new MessageButton(MessageButton::TYPE_POSTBACK, 'Second button')
-                                        ])
-                                    ]
-                                ]
-                            ));
-
-                            break;
-
-                        // When bot receive "receipt"
-                        case 'receipt':
-
-                            $bot->send(new StructuredMessage($message['sender']['id'],
-                                StructuredMessage::TYPE_RECEIPT,
-                                [
-                                    'recipient_name' => 'Fox Brown',
-                                    'order_number' => rand(10000, 99999),
-                                    'currency' => 'USD',
-                                    'payment_method' => 'VISA',
-                                    'order_url' => 'http://facebook.com',
-                                    'timestamp' => time(),
-                                    'elements' => [
-                                        new MessageReceiptElement("First item", "Item description", "", 1, 300, "USD"),
-                                        new MessageReceiptElement("Second item", "Item description", "", 2, 200, "USD"),
-                                        new MessageReceiptElement("Third item", "Item description", "", 3, 1800, "USD"),
-                                    ],
-                                    'address' => new Address([
-                                        'country' => 'US',
-                                        'state' => 'CA',
-                                        'postal_code' => 94025,
-                                        'city' => 'Menlo Park',
-                                        'street_1' => '1 Hacker Way',
-                                        'street_2' => ''
-                                    ]),
-                                    'summary' => new Summary([
-                                        'subtotal' => 2300,
-                                        'shipping_cost' => 150,
-                                        'total_tax' => 50,
-                                        'total_cost' => 2500,
-                                    ]),
-                                    'adjustments' => [
-                                        new Adjustment([
-                                            'name' => 'New Customer Discount',
-                                            'amount' => 20
-                                        ]),
-
-                                        new Adjustment([
-                                            'name' => '$10 Off Coupon',
-                                            'amount' => 10
-                                        ])
-                                    ]
-                                ]
-                            ));
-
-                            break;
-
-                        // Other message received
-                        default:
-                            $bot->send(new Message($message['sender']['id'], 'Sorry. I don’t understand you.'));
-                    }
                 }
             }
         }
 
-        return new Response();
+        //return new Response();
 
     }
 }
